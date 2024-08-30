a40f1779592c5f770f200607e96af7919bde85e6
ToyohikoUchida <tyhkucd@gmail.com>
2024-08-30 12:04:09 +0900

    Initial Commit

---
+++ SimpleLazyVGrid/SimpleLazyVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLazyVGridApp.swift
+//  SimpleLazyVGrid
+//
+//  Created by 内田豊彦 on 2024/08/30.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLazyVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
